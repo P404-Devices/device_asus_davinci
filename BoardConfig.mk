fb459f674866d6531c41e028c61e8356b69763da
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2023-02-13 00:15:55 -0500

    davinci: Drop compat matrix definition
    * Since we use all QCOM components, it's automatically defined and copied. [1]
    
    [1]: https://gerrit.aospa.co/c/AOSPA/android_device_qcom_common/+/24623
    
    Change-Id: Ic2479a4e8df6498cdd2c3d11b04e6dd8c5cf8017

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,9 +73,6 @@ DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/asus_manifest.xml \
     $(DEVICE_PATH)/manifest.xml
 
-DEVICE_MATRIX_FILE += \
-    device/qcom/common/compatibility_matrix.xml
-
 # Hacks
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 BUILD_BROKEN_DUP_RULES := true
